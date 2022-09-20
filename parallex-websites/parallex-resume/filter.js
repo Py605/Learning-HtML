046c628875b5635b3d41886ab42e99a293d6dfe5
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-09-20 19:20:02 +0530

    updated responsiveness/ completed!!

--- parallex-websites/parallex-resume/filter.js
+++ parallex-websites/parallex-resume/filter.js
@@ -1,3 +1,20 @@
+// for Navbar..
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
+// ---Project Section---
 let sortBtn = document.querySelector('.filter-menu').children;
 let sortItem = document.querySelector('.filter-item').children;
 
@@ -21,5 +38,28 @@ for(let i = 0; i < sortBtn.length; i++){
                 sortItem[k].classList.add('active');
             }
         }
+    });
+}
+
+// --- Contact form----
+
+function validateForm(){
+
+    //reseting the whole form...
+    document.querySelector(".input-form").reset();
+
+    // showing the success message..
+    document.querySelector(".success-message").innerHTML += "<p id='success'></p><i class='fa-solid fa-xmark'></i>";
+
+    document.querySelector('#success').innerText= "Your message was sent successfully. Thanks!😊";
+
+
+    var close_btn = document.querySelector('.fa-xmark');
+    // now, removing whole success-message's content while clicking on close-button...
+    close_btn.addEventListener('click',function(){
+        var elem = document.getElementById("success");
+        elem.remove();
+        var elem2 = document.querySelector(".fa-xmark");
+        elem2.remove();
     });
 }
